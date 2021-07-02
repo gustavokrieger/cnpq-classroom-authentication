10a66b13af0f826c239e74a5fdd4d0446c164b56
gustavo <gustavopcassol@gmail.com>
2021-07-01 22:43:35 -0300

    reset backend with older django version

--- backend/base/shib.py
+++
@@ -1,5 +0,0 @@
-from django.contrib.auth.middleware import RemoteUserMiddleware
-
-
-class CustomHeaderMiddleware(RemoteUserMiddleware):
-    header = "HTTP_REMOTE_USER"
--- backend/base/views.py
+++ backend/base/views.py
@@ -1,17 +1,3 @@
-from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
-from django.shortcuts import render, redirect
-from django.template import loader
+from django.shortcuts import render
 
-
-def index(request):
-    if request.user.is_authenticated:
-        return redirect("/users")
-    return render(request, "base/index.html")
-
-
-@login_required
-def users(request):
-    template = loader.get_template("base/users.html")
-    meta = request.META
-    return HttpResponse(template.render(meta, request))
+# Create your views here.
