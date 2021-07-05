761cd04fd9afd3d0de1f17c94aeb15ef64e313a2
gustavo <gustavopcassol@gmail.com>
2021-07-05 00:36:27 -0300

    add query string example in view

--- backend/base/views.py
+++ backend/base/views.py
@@ -3,12 +3,14 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.template import loader
+from django.utils.http import urlencode
 
 
 def index(request):
     if request.user.is_authenticated:
         # return redirect("/users")
-        return redirect(f"http://{settings.DOMAIN}:3000/")
+        query_string = urlencode({"username": "username"})
+        return redirect(f"http://{settings.DOMAIN}:3000/?{query_string}")
     return render(request, "base/index.html")
 
 
