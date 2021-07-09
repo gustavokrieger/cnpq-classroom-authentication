ec29b47559c5d7cbc014e66d024b7bb9c965135a
gustavo <gustavopcassol@gmail.com>
2021-07-09 01:26:56 -0300

    add TemporaryToken model

--- backend/base/models.py
+++ backend/base/models.py
@@ -1,3 +1,16 @@
-from django.db import models
+from datetime import timedelta
 
-# Create your models here.
+from django.db import models
+from django.utils.timezone import now
+from rest_framework.authtoken.models import Token
+
+from base.managers import TemporaryTokenManager
+
+
+class TemporaryToken(Token):
+    lifespan = models.DurationField(default=timedelta(seconds=900))
+
+    objects = TemporaryTokenManager()
+
+    def has_expired(self):
+        return now() > self.created + self.lifespan
