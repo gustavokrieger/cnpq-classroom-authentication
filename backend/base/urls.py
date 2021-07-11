8d56a5dfb678fb19a2aa97bc1e835ebbac28d7da
gustavo <gustavopcassol@gmail.com>
2021-07-10 23:20:46 -0300

    create TokenExchangeView view

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+
 from . import views
 
 app_name = "base"
 urlpatterns = [
     path("", views.index, name="index"),
     path("users/", views.users, name="users"),
+    path("token-exchange/", views.TokenExchangeView.as_view(), name="token-exchange"),
 ]
--- backend/base/views.py
+++ backend/base/views.py
@@ -1,15 +1,19 @@
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseBadRequest
 from django.shortcuts import render, redirect
 from django.template import loader
 from django.utils.http import urlencode
+from django.views import View
+
+from base.models import TemporaryToken
 
 
 def index(request):
     if request.user.is_authenticated:
         # return redirect("/users")
-        query_string = urlencode({"username": "username"})
+        temporary_token = TemporaryToken.objects.create_or_replace(request.user)
+        query_string = urlencode({"temporary_token": temporary_token})
         return redirect(f"http://{settings.DOMAIN}:3000/?{query_string}")
     return render(request, "base/index.html")
 
@@ -19,3 +23,12 @@ def users(request):
     template = loader.get_template("base/users.html")
     meta = request.META
     return HttpResponse(template.render(meta, request))
+
+
+class TokenExchangeView(View):
+    def post(self, request):
+        temporary_token_key = request.POST.get("temporary_token")
+        if not temporary_token_key:
+            return HttpResponseBadRequest('Missing "temporary_token" in POST data.')
+        token = TemporaryToken.objects.exchange(temporary_token_key)
+        return HttpResponse(token)
