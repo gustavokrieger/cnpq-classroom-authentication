7a13f1805dddc3ebbea34a2e98fbed11a9f1b9a0
gustavo <gustavopcassol@gmail.com>
2021-07-17 01:38:08 -0300

    add CourseClass and Presence models

--- backend/base/models.py
+++ backend/base/models.py
@@ -1,11 +1,14 @@
 from datetime import timedelta
 
+from django.contrib.auth import get_user_model
 from django.db import models
 from django.utils import timezone
 from rest_framework.authtoken.models import Token
 
 from base.exceptions import InvalidTemporaryTokenError
 from base.managers import TemporaryTokenManager
+
+User = get_user_model()
 
 
 class TemporaryToken(Token):
@@ -19,3 +22,15 @@ class TemporaryToken(Token):
 
     def _has_expired(self):
         return timezone.now() > self.created + self.lifespan
+
+
+class CourseClass(models.Model):
+    users = models.ManyToManyField(User, related_name="course_classes", through="Presence")
+    course = models.CharField(max_length=50)
+    start = models.TimeField()
+    end = models.TimeField()
+
+
+class Presence(models.Model):
+    user = models.ForeignKey(User, models.PROTECT)
+    course_class = models.ForeignKey(CourseClass, models.PROTECT)
