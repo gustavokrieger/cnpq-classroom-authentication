1b43448633afebee2bcdae449f9269241301c2d9
gustavo <gustavopcassol@gmail.com>
2021-07-30 00:52:47 -0300

    add request to get and display lectures

--- frontend/src/pages/SubjectList.tsx
+++ frontend/src/pages/SubjectList.tsx
@@ -1,32 +1,44 @@
 import "./SubjectList.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ListGroup from "react-bootstrap/ListGroup";
 import Container from "react-bootstrap/Container";
 import ConfirmationModal from "../components/ConfirmationModal";
 
+interface Lecture {
+  course: string;
+  is_ongoing: boolean;
+}
+
 export default function SubjectList(): JSX.Element {
   const [showConfirmation, setShowConfirmation] = useState(false);
+  const [lectures, setLectures] = useState<readonly Lecture[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      const init = {
+        headers: {
+          Authorization: "Token f1889b9a70431f0c285230272163af0a61d2916e",
+        },
+      };
+      const response = await fetch("http://127.0.0.1:8000/api/lectures/", init);
+      setLectures(await response.json());
+    })();
+  }, []);
 
   const handleConfirmationAccept = () => {
     setShowConfirmation(false);
     // TODO: make request here.
   };
 
-  const items = [
-    { subject: "materia 1", disabled: true },
-    { subject: "materia 2", disabled: false },
-    { subject: "materia 3", disabled: true },
-  ];
-
-  const ListGroupItems = items.map((value, index) => (
+  const ListGroupItems = lectures.map((v, i) => (
     <ListGroup.Item
       className="list-group__item"
-      key={index}
+      key={i}
       action
       onClick={() => setShowConfirmation(true)}
-      disabled={value.disabled}
+      disabled={!v.is_ongoing}
     >
-      {value.subject}
+      {v.course}
     </ListGroup.Item>
   ));
 
