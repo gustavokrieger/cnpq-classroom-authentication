22f2b73279c49bd05142c39e87f3c5aa12bb7d7c
gustavo <gustavopcassol@gmail.com>
2021-08-03 01:07:44 -0300

    change database to be postgresql

--- backend/sp_django/settings.py
+++ backend/sp_django/settings.py
@@ -112,8 +112,12 @@ WSGI_APPLICATION = "sp_django.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB", "postgres"),
+        "USER": os.getenv("POSTGRES_USER", "postgres"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "postgres"),
+        "HOST": os.getenv("DATABASE_HOST", "127.0.0.1"),
+        "PORT": 5432,
     }
 }
 
