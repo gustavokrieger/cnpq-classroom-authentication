a059f8889e41451b3f0cfaa6c229bf95787dc0cd
gustavo <gustavopcassol@gmail.com>
2021-08-05 00:04:12 -0300

    remove dead code

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from rest_framework.routers import SimpleRouter
 
-from base.views import index, users, TokenExchangeView, LectureViewSet, UserView
+from base.views import TokenExchangeView, LectureViewSet, UserView
 
 app_name = "base"
 
@@ -9,8 +9,6 @@ router = SimpleRouter()
 router.register(r"lectures", LectureViewSet, basename="lecture")
 
 urlpatterns = [
-    path("", index, name="index"),
-    path("users/", users, name="users"),
     path("token-exchange/", TokenExchangeView.as_view(), name="token-exchange"),
     path("user/", UserView.as_view(), name="user"),
 ] + router.urls
--- backend/base/views.py
+++ backend/base/views.py
@@ -1,13 +1,7 @@
 from datetime import date
 
-from django.conf import settings
 from django.contrib.auth import get_user_model
-from django.contrib.auth.decorators import login_required
 from django.db import IntegrityError
-from django.http import HttpResponse
-from django.shortcuts import render, redirect
-from django.template import loader
-from django.utils.http import urlencode
 from rest_framework import permissions, mixins, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
@@ -23,22 +17,6 @@ from base.serializers import (
 )
 
 User = get_user_model()
-
-
-def index(request):
-    if request.user.is_authenticated:
-        # return redirect("/users")
-        temporary_token = TemporaryToken.objects.create_or_replace(request.user)
-        query_string = urlencode({"temporary_token": temporary_token})
-        return redirect(f"http://{settings.DOMAIN}:3000/?{query_string}")
-    return render(request, "base/index.html")
-
-
-@login_required
-def users(request):
-    template = loader.get_template("base/users.html")
-    meta = request.META
-    return HttpResponse(template.render(meta, request))
 
 
 class TokenExchangeView(APIView):
