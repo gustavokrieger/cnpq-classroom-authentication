7d805c7f3a9acc9e2725da13235ca3f547ed1f93
gustavo <gustavopcassol@gmail.com>
2021-08-11 01:35:45 -0300

    navbar and margin adjustments

--- frontend/src/components/MainNavbar.tsx
+++ frontend/src/components/MainNavbar.tsx
@@ -4,7 +4,7 @@ import univaliLogo from "../images/univaliLogo.png";
 
 export default function MainNavbar(): JSX.Element {
   return (
-    <Navbar className="navbar">
+    <Navbar className="navbar" fixed="top">
       <Navbar.Brand href="/">
         <img src={univaliLogo} width="104" height="71" alt="Univali logo" />
       </Navbar.Brand>
