378c30efb83d32c130e344bdd4db118dcd6bd0dd
gustavo <gustavopcassol@gmail.com>
2021-08-15 17:25:50 -0300

    make multiple attendance requests instead of one

--- frontend/src/pages/SubjectList.tsx
+++ frontend/src/pages/SubjectList.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import ListGroup from "react-bootstrap/ListGroup";
 import ConfirmationModal from "../components/ConfirmationModal";
 import { attendLecture, loadLectures } from "../external/backend";
+import Toast from "react-bootstrap/Toast";
 
 interface Lecture {
   id: number;
@@ -10,17 +11,31 @@ interface Lecture {
   start: string;
   end: string;
   is_ongoing: boolean;
-  has_attended: boolean;
 }
 
 export default function SubjectList(): JSX.Element {
   const [showConfirmation, setShowConfirmation] = useState(false);
+  const [showToast, setShowToast] = useState(false);
   const [lectures, setLectures] = useState<readonly Lecture[]>([]);
   const [selectedLectureId, setSelectedLectureId] = useState(0);
+  const [attendingLectureId, setAttendingLectureId] = useState(0);
 
   useEffect(() => {
     loadAndSetLectures();
   }, []);
+
+  useEffect(() => {
+    if (!attendingLectureId) {
+      return;
+    }
+
+    const interval = setInterval(() => {
+      attendLecture(attendingLectureId);
+      setShowToast(true);
+    }, 5_000);
+
+    return () => clearInterval(interval);
+  }, [attendingLectureId]);
 
   const loadAndSetLectures = async () => {
     const response = await loadLectures();
@@ -29,12 +44,11 @@ export default function SubjectList(): JSX.Element {
 
   const handleConfirmationAccept = async () => {
     setShowConfirmation(false);
-    await attendLecture(selectedLectureId);
-    await loadAndSetLectures();
+    setAttendingLectureId(selectedLectureId);
   };
 
   const getListGroupProps = (lecture: Lecture) => {
-    if (lecture.has_attended) {
+    if (lecture.id === attendingLectureId) {
       return {
         variant: "success",
       };
@@ -67,11 +81,6 @@ export default function SubjectList(): JSX.Element {
 
   return (
     <>
-      <ConfirmationModal
-        show={showConfirmation}
-        handleClose={() => setShowConfirmation(false)}
-        handleAccept={handleConfirmationAccept}
-      />
       <h1 className="title-heading">registro de presença</h1>
       {lectures.length === 0 ? (
         <h5 className="list-heading text-muted">
@@ -80,11 +89,30 @@ export default function SubjectList(): JSX.Element {
       ) : (
         <>
           <h5 className="list-heading text-muted">
-            selecione aula para registrar presença.
+            selecione aula para começar a registrar presença.
           </h5>
           <ListGroup className="list-group">{listGroupItems}</ListGroup>
         </>
       )}
+      <ConfirmationModal
+        show={showConfirmation}
+        handleClose={() => setShowConfirmation(false)}
+        handleAccept={handleConfirmationAccept}
+      />
+      <Toast
+        className="attended-toast"
+        onClose={() => setShowToast(false)}
+        show={showToast}
+        delay={3_000}
+        autohide
+      >
+        <Toast.Header className="attended-toast__header" closeButton={false}>
+          <strong>aviso</strong>
+        </Toast.Header>
+        <Toast.Body className="attended-toast__body">
+          sua presença na aula foi registrada.
+        </Toast.Body>
+      </Toast>
     </>
   );
 }
