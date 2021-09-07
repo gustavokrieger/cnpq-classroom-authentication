928ceaaa49a1b27b1dc1d4d7398e1a8e14495be8
gustavo <gustavopcassol@gmail.com>
2021-09-07 15:52:51 -0300

    add text to main navbar

--- frontend/src/components/MainNavbar.tsx
+++ frontend/src/components/MainNavbar.tsx
@@ -1,13 +1,28 @@
 import "./MainNavbar.css";
 import Navbar from "react-bootstrap/Navbar";
 import univaliLogo from "../images/univaliLogo.png";
+import Button from "react-bootstrap/Button";
 
 export default function MainNavbar(): JSX.Element {
   return (
-    <Navbar className="navbar" fixed="top">
+    <Navbar className="main-navbar" fixed="top">
       <Navbar.Brand href="/">
-        <img src={univaliLogo} width="104" height="71" alt="Univali logo" />
+        <img
+          src={univaliLogo}
+          width={104 * 0.8}
+          height="auto"
+          alt="Univali logo"
+        />
       </Navbar.Brand>
+      <Navbar.Collapse className="justify-content-end">
+        <Navbar.Text className="navbar-text">
+          <span className="navbar-text__span">Aluno</span>
+          <br />
+          <Button className="navbar-text__button" variant="link">
+            Logout
+          </Button>
+        </Navbar.Text>
+      </Navbar.Collapse>
     </Navbar>
   );
 }
